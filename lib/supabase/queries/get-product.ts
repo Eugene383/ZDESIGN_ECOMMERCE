2537ec5e89df656f73fe626a554658a503c17da8
Eugénio Adão Teca <adaoeugenio383@gmail.com>
2026-08-22 00:49:53 -1200

    alterar a query do product

--- lib/supabase/queries/get-product.ts
+++ lib/supabase/queries/get-product.ts
@@ -4,9 +4,9 @@ export async function getProduct(id:string){
     const supabase = await createClient()
     const { data, error } = await supabase
     .from("products").
-    select(`*, product_images (id, image_url, is_primary), 
-        product_variants(id,sku,color,size,stock,price)
-        ,product_attributes(id,attribute_name,attribute_value)`)
+    select(`*, product_images (id, image_url, is_primary,product_id), 
+        product_variants(id,sku,color,size,stock,price,product_id)
+        ,product_attributes(id,attribute_name,attribute_value,product_id)`)
     .eq("id", id)
     .order("is_primary", { foreignTable: "product_images", ascending: false })
     .maybeSingle()
